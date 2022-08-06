d2f67c46a48449e5aa0d93872fa1d629553cde9b
ivani2 <eltalivanitodos@gmail.com>
2022-08-06 18:21:02 -0500

    ultimo cambio fin de seccion 15

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Auth } from 'src/app/auth/interfaces/auth.interface';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
